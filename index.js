d8af763c858b4cd04872ed01eadbe5f7fa23395c
Stephanie Boie <stephanieboie10@gmail.com>
2023-03-18 12:10:24 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
-}
+  let result;
+  if (distance <= 400) {
+    result = 'This one is on me!'
+  }
+    else if (distance > 400 && distance <2000) { 
+      result = 'That will be twenty bucks.'
+    }
+    else if (distance > 2000 && distance <2500) {
+        result = 'I will gladly take your thirty bucks.'
+      }
+      else if (distance > 2500) {
+        result = 'No can do.'
+      }
+  return result
+  }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  const result = city === 'NYC' ? "Ok, sounds good." : "No go.";
+  return result
 }
+function switchOnCharmFromTip(tip){
+  // Write your code here!'
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  let result;
+
+  switch (tip) {
+    case 'generous':
+      result = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      result = 'Thank you.';
+      break;
+    default: 
+      result = 'Bye.';
+  }
+  return result
 }
